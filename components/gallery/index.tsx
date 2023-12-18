d713912248b4bc78dcf2b88ee0d097ea6876d7c6
Mohamed Ahmed <knightwing862@gmail.com>
2023-12-18 16:20:09 +0200

    Final Changes

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,38 +1,41 @@
 "use client";
 
-import Image from "next/image";
+import NextImage from "next/image";
 import { Tab } from "@headlessui/react";
-import { Image as ImageType } from "@/types";
+
+import { Image } from "@/types";
+
 import GalleryTab from "./gallery-tab";
 
 interface GalleryProps {
-  images: ImageType[];
+  images: Image[];
 }
 
-const Gallery: React.FC<GalleryProps> = ({ images }) => {
+const Gallery: React.FC<GalleryProps> = ({ images = [] }) => {
   return (
-    <div className="mobile-gallery">
-      <Tab.Group as="div" className="flex flex-col-reverse">
-        <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-          <Tab.List className="grid grid-cols-4 gap-6">
-            {images.map((image) => (
-              <GalleryTab key={image.id} image={image} />
-            ))}
-          </Tab.List>
-        </div>
-        <Tab.Panels className="aspect-square w-full ">
+    <Tab.Group as="div" className="flex flex-col-reverse">
+      <div className="mx-auto mt-6 w-full max-w-2xl sm:block lg:max-w-none">
+        <Tab.List className="grid grid-cols-4 gap-6">
           {images.map((image) => (
-            <Tab.Panel key={image.id}>
-              <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
-                <div className="absolute inset-0">
-                  <Image fill objectFit="cover" src={image.url} alt="Image" />
-                </div>
-              </div>
-            </Tab.Panel>
+            <GalleryTab key={image.id} image={image} />
           ))}
-        </Tab.Panels>
-      </Tab.Group>
-    </div>
+        </Tab.List>
+      </div>
+      <Tab.Panels className="aspect-square w-full">
+        {images.map((image) => (
+          <Tab.Panel key={image.id}>
+            <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+              <NextImage
+                fill
+                src={image.url}
+                alt="Image"
+                className="object-cover object-center"
+              />
+            </div>
+          </Tab.Panel>
+        ))}
+      </Tab.Panels>
+    </Tab.Group>
   );
 };
 
